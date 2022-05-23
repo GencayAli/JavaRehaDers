e6b65bb98c12961ab41db80a62758fe1210de78f
Mehmet Ali Gencay <gencayali20@gmail.com>
2022-05-23 22:05:35 +0200

    Initial commit

---
+++ src/Hallo.java
@@ -0,0 +1,9 @@
+public class Hallo {
+    public static void main(String[] args) {
+
+        System.out.println("hallo java");
+        System.out.println("Merhaba Dünya");
+        System.out.println("fhkimndofinmsldfh");
+
+    }
+}
---
+++ src/degiskenler/Degiskenler.java
@@ -0,0 +1,21 @@
+package degiskenler;
+
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+
+public class Degiskenler {
+    public static void main(String[] args) {
+        int sayi = 4;
+        String metin ="Degisken";
+        double ondalik = 2.5;
+        float ondalik2 = 2.4f;
+        String adSoyad = "Mehmet Ali Gencay";
+        int yas = 36;
+
+        System.out.println(sayi);
+        System.out.println(ondalik);
+        System.out.println(metin);
+        System.out.println(ondalik2);
+        System.out.println(adSoyad);
+        System.out.println(yas);
+    }
+}
